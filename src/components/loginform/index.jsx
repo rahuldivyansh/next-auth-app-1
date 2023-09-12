415668a51ccad1b8f0a5227a41d4496d2ff2f8c3
rahuldivyansh <90146156+rahuldivyansh@users.noreply.github.com>
2023-09-12 21:22:16 +0530

    fixed bugs in build

--- src/components/loginform/index.jsx
+++ src/components/loginform/index.jsx
@@ -35,22 +35,22 @@ const LoginForm = () => {
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400 ">
-        <h1 className="text-xl font-bold my-4">Enter your login details</h1>
+        <h1 className="text-xl font-bold my-4">{`Enter your login details`}</h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input type="email" placeholder="Email"  onChange={(e) => setEmail(e.target.value)}/>
           <input type="password" placeholder="Password"  onChange={(e) => setPassword(e.target.value)}/>
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-            Login
+            {`Login`}
           </button>
 
           {error && (<div className="bg-red-500 text-white w-fit text-sm py-1 px-2 rounded mt-2">
             {error}
           </div>)}
           <p className="text-sm mt-3 text-right">
-            Don't have an account ?{" "}
+            {`Don't have an account ?`}{" "}
             <Link href={"/register"} className="underline">
-              Register here
+              {`Register here`}
             </Link>
           </p>
         </form>
